977c2704511c1103cd2abea5df13540545db09ea
rahmanshaon <rahmanshaon45@gmail.com>
2025-10-06 18:24:14 +0600

    default export added in routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router";
-import { router } from "./routes/Routes.jsx";
+import router from "./routes/Routes.jsx";
+// import { router } from "./routes/Routes.jsx";
 // import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")).render(
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router";
 import App from "../App";
 
 // named export
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
 ]);
+
+export default router;
