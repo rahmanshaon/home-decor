c658176311ac8977299b2bc4db51559a0105c28f
rahmanshaon <rahmanshaon45@gmail.com>
2025-10-06 19:13:05 +0600

    error page updated

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { useRouteError } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const ErrorPage = () => {
-    const error = useRouteError()
+  const error = useRouteError();
+  const message = error?.statusText || error?.message;
+  const status = error?.status;
   return (
     <div>
-      <h1>{error.message}</h1>
+      <Navbar />
+      {/* <p className="text-xl text-red-600">{error.message}</p> */}
+      <h1 className="text-7xl font-extrabold text-gray-800 mb-4">{status}</h1>
+      <p className="text-2xl font-semibold text-red-600 mb-2">{message}</p>
+      <Footer />
     </div>
   );
 };
